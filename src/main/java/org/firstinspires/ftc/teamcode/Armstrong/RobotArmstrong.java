b55b922330e04d824282110b955b86648bd3dbcd
Gent Semaj <gentsemaj@gentasaur.us>
2018-11-11 18:25:48 -0500

    Auto Elevator Update

--- src/main/java/org/firstinspires/ftc/teamcode/Armstrong/RobotArmstrong.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Armstrong/RobotArmstrong.java
@@ -4,25 +4,35 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class RobotArmstrong {
-    //Declares the motor that controls the left side of the drivetrian
+    //Declares the motors that control the left and right side of the drivetrian
     public DcMotor left;
-    //Declares the motor that controls the right side of the drivetrian
     public DcMotor right;
     //Declares the motor that controls the hanging mechanism of the robot
     public DcMotor hanger;
     //Declares the motor that controls the arm of the robot
-    public DcMotor armRotate;
+    public DcMotor arm;
+
+    //Declares the touch sensors that limit the elevator motion
+    public TouchSensor lowerTouch;
+    public TouchSensor upperTouch;
 
     public void init(HardwareMap map){
         right = map.dcMotor.get("right");
         right.setDirection(DcMotorSimple.Direction.REVERSE);
         left = map.dcMotor.get("left");
         hanger = map.dcMotor.get("hanger");
-        armRotate = map.dcMotor.get("arm");
+        hanger.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm = map.dcMotor.get("arm");
+
+        lowerTouch = map.touchSensor.get("lowerTouch");
+        upperTouch = map.touchSensor.get("upperTouch");
     }
+
     /*
     Sets the power of the motors to 0, thus stopping the motion of the robot
      */
@@ -30,6 +40,6 @@ public class RobotArmstrong {
         left.setPower(0);
         right.setPower(0);
         hanger.setPower(0);
-        armRotate.setPower(0);
+        arm.setPower(0);
     }
 }
--- src/main/java/org/firstinspires/ftc/teamcode/Armstrong/TeleOp/ArmstrongDrive.java
+++ src/main/java/org/firstinspires/ftc/teamcode/Armstrong/TeleOp/ArmstrongDrive.java
@@ -9,12 +9,27 @@ import org.firstinspires.ftc.teamcode.Armstrong.RobotArmstrong;
 @TeleOp(name = "ArmstrongDrive", group = "Armstrong")
 public class ArmstrongDrive extends OpMode {
 
-    RobotArmstrong robot;
+    private RobotArmstrong robot;
+
+    //position of the left gamepad1 stick
+    double lVal;
+    //position of the right gamepad1 stick
+    double rVal;
+    //position of the left gamepad2 stick
+    double armVal;
+
+    //elevator state variables
+    boolean goingUp;
+    boolean goingDown;
 
     /*
     Establishes the robot and initializes the robot using the hardwareMap
      */
     public void init() {
+
+        goingDown = false;
+        goingUp = false;
+
         robot = new RobotArmstrong();
         robot.init(hardwareMap);
     }
@@ -26,16 +41,14 @@ public class ArmstrongDrive extends OpMode {
     public void loop() {
         updateDriver(gamepad1);
         updateCoDriver(gamepad2);
+        updateAuto();
+        printStats();
     }
 
-    final double maxPower = 0.8;
+    final static double maxPower = 0.8;
+    final static double elevatorSpeed = 0.6;
 
-    //Variable for the position of the left gamepad stick
-    double lVal;
-    //Variable for the position of the right gamepad stick
-    double rVal;
-
-    public void updateDriver(Gamepad gamepad) {
+    private void updateDriver(Gamepad gamepad) {
         //Check drive train controls and updates accordingly
         lVal = gamepad.left_stick_y;
         rVal = gamepad.right_stick_y;
@@ -46,23 +59,60 @@ public class ArmstrongDrive extends OpMode {
 
         //Moves the hanging mechanism up and dowm depending on the trigger positions
         if(gamepad.right_trigger > 0 && gamepad.left_trigger == 0)
-            robot.hanger.setPower(gamepad.right_trigger);
+        {
+            goingUp = false;
+            goingDown = true;
+        }
         else if(gamepad.right_trigger == 0 && gamepad.left_trigger > 0)
-            robot.hanger.setPower(-gamepad.left_trigger);
-        else
-            robot.hanger.setPower(0);
+        {
+            goingUp = true;
+            goingDown = false;
+        }
 
     }
 
-    /*
-    Extends and Retracts arm on the robot depending on the stick placement using the second controller
-     */
-    public void updateCoDriver(Gamepad gamepad) {
+    private void updateCoDriver(Gamepad gamepad) {
         //Checks gamepad controls and updates accordingly
-        lVal = gamepad.left_stick_y;
+        armVal = gamepad.left_stick_y;
 
         //Moves robot arm according to the left sticks position
-        robot.armRotate.setPower(lVal * maxPower);
+        robot.arm.setPower(armVal * 0.5);
+    }
+
+    private void updateAuto() {
+        if(goingUp)
+        {
+            if(robot.upperTouch.isPressed())
+            {
+                goingUp = false;
+                goingDown = false;
+                robot.hanger.setPower(0);
+            }
+            else
+                robot.hanger.setPower(elevatorSpeed);
+        }
+        else if(goingDown)
+        {
+            if(robot.lowerTouch.isPressed())
+            {
+                goingUp = false;
+                goingDown = false;
+                robot.hanger.setPower(0);
+            }
+            else
+                robot.hanger.setPower(-elevatorSpeed);
+        }
+        else
+            robot.hanger.setPower(0);
+    }
+
+    public void printStats()
+    {
+        if(robot != null)
+        {
+            telemetry.addData("upper", robot.upperTouch.isPressed() ? "pressed" : "unpressed");
+            telemetry.addData("lower", robot.lowerTouch.isPressed() ? "pressed" : "unpressed");
+        }
     }
 
 }
