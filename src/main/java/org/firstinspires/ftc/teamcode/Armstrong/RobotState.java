62c4919895518f2044738bc2513d40bd2b2c2488
Kirk Brauer <kirkbrauer@outlook.com>
2018-11-11 20:26:40 -0500

    Add BasicAutonomous program

---
+++ src/main/java/org/firstinspires/ftc/teamcode/Armstrong/RobotState.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Armstrong;
+
+/**
+ * Represents the state of the robot.
+ */
+public enum RobotState {
+    START,
+    LOWER,
+    UNHOOK,
+    BACKUP,
+    DRIVE_AWAY
+}
